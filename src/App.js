7ba7ea1a8873b6d7854981ad13b6f3382060b55c
joypauls <joypaulsen3@gmail.com>
2021-09-22 18:01:29 -0500

    chakra ui framework

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {
   ChakraProvider,
+  Flex,
+  Container,
   Box,
   Text,
   Link,
@@ -12,10 +14,22 @@ import {
 import { ColorModeSwitcher } from './ColorModeSwitcher';
 import { Logo } from './Logo';
 
+import WithSubnavigation from "./navbar";
+import SmallWithSocial from "./footer";
+
 function App() {
   return (
     <ChakraProvider theme={theme}>
-      <Box textAlign="center" fontSize="xl">
+      <Flex direction="column" minHeight="100vh">
+        <WithSubnavigation/>
+        <Flex flex="1" direction="column" justifyContent="center">
+          <Container>
+            This is content.
+          </Container>
+        </Flex>
+        <SmallWithSocial/>
+      </Flex>
+      {/* <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
           <VStack spacing={8}>
@@ -34,7 +48,7 @@ function App() {
             </Link>
           </VStack>
         </Grid>
-      </Box>
+      </Box> */}
     </ChakraProvider>
   );
 }
